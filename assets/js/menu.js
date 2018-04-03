1dcf999cb2dd1292902b00919e5ddfa5491aee89
Amjad Rathod <amjadrathod@gmail.com>
2018-04-03 19:47:18 +0530

    add some page

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,32 +1,38 @@
 function myFunction(x) {
     x.classList.toggle("change");
 }
-
-// $(document).ready(function(){
-//
-//     $.keyframe.define([{
-//     name: 'left-icon',
-//     '0%': {
-//         'top' : '1px',
-//         'width' : '3px',
-//         'left' : '-0.5px'
-//     },
-//     '100%': {
-//         'top' : '20px',
-//         'width' : '420px',
-//         'left' : '-20px'
-//     }
-//     }
-//     ]);
-//
-//     $(".menu-icon").hover(function(){
-//         $(".icon-left-1").playKeyframe(
-//             'left-icon 1s',
-//             complete
-//         );
-//     },
-//
-//         function(){
-//         $(this).css("background-color", "pink");
-//     });
-// });
+$(document).ready(function(){
+    $('.menu, nav').click(function(){
+    if ($('.menu').hasClass('active')) {
+      $('nav ul').removeClass('active');
+      $('nav').delay(800).fadeOut('slow');
+      setTimeout(function() {
+        $('.menu').removeClass('active');
+        $('.section-wrap').removeClass('active');
+       }, 800);
+    } else {
+      $('.menu').addClass('active');
+      $('nav').fadeIn('slow');
+      $('nav').addClass('active');
+      $('nav ul').addClass('active');
+      $('.section-wrap').addClass('active');
+    }
+  });
+  $('.menu-bar').click(function(){
+    // $('.toggle-menu').css({
+    //   'top' : '0',
+    //   'right' : '0',
+    //   'height' : '100vh',
+    //   'transition' : 'all 0.5s'
+    // });
+    // $('.menu-bar').css({"background":"green"});
+    if($('.menu-bar').hasClass('back')){
+      
+    }
+  });
+});
+//top: 0;
+//   right: 0;
+//   height: 100vh;
+//   width: 50%;
+//   transition: all .5s;
