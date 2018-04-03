6198191f25a3d0707715bf704a904e07e49d105a
Amjad Rathod <amjadrathod@gmail.com>
2018-04-03 16:24:29 +0530

    menu icon bug fixed

---
+++ assets/js/menu.js
@@ -0,0 +1,32 @@
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+// $(document).ready(function(){
+//
+//     $.keyframe.define([{
+//     name: 'left-icon',
+//     '0%': {
+//         'top' : '1px',
+//         'width' : '3px',
+//         'left' : '-0.5px'
+//     },
+//     '100%': {
+//         'top' : '20px',
+//         'width' : '420px',
+//         'left' : '-20px'
+//     }
+//     }
+//     ]);
+//
+//     $(".menu-icon").hover(function(){
+//         $(".icon-left-1").playKeyframe(
+//             'left-icon 1s',
+//             complete
+//         );
+//     },
+//
+//         function(){
+//         $(this).css("background-color", "pink");
+//     });
+// });
