038cf341f0d13f8ea5918b45e73c91d227131a0f
Amjad Rathod <amjad@fatah.co>
2018-04-14 13:18:45 +0530

    create contact page

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -110,8 +110,21 @@ function menuBarClick() {
     $('.brand-icon').removeClass('close-menu-brand-icon');
   }
 }
+
 $(document).ready(function() {
   $(".menu-bar").bind( "click", menuBarClick );
   currentUrl();
   menuLinkClick();
+//   var elements = document.getElementsByTagName("input");
+//   for (var i = 0; i < elements.length; i++) {
+//     elements[i].oninvalid = function(e) {
+//         e.target.setCustomValidity("");
+//         if (!e.target.validity.valid) {
+//             e.target.setCustomValidity("This field cannot be left blank");
+//         }
+//     };
+//     elements[i].oninput = function(e) {
+//         e.target.setCustomValidity("");
+//     };
+// }
 });
