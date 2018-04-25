09ae7c189f3789bd3a195a2e62fda81c537306e6
Amjad Rathod <amjad@fatah.co>
2018-04-25 12:04:33 +0530

    upload file name bug fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -480,7 +480,28 @@ function projectChkBox() {
       // $('#chk-2').addClass('check_border');
   });
 }
+function fileUpload() {
+  var inputs = document.querySelectorAll( '.inputfile' );
+  Array.prototype.forEach.call( inputs, function( input )
+  {
+  	var label	 = input.nextElementSibling,
+  		labelVal = label.innerHTML;
 
+  	input.addEventListener( 'change', function( e )
+  	{
+  		var fileName = '';
+  		if( this.files && this.files.length > 1 )
+  			fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+  		else
+  			fileName = e.target.value.split( '\\' ).pop();
+
+  		if( fileName )
+  			label.querySelector( 'span' ).innerHTML = fileName;
+  		else
+  			label.innerHTML = labelVal;
+  	});
+  });
+}
 $(document).ready(function() {
   $(".menu-bar").bind( "click", menuBarClick );
   currentUrl();
@@ -488,7 +509,7 @@ $(document).ready(function() {
   contactFormValidation();
   removeHref();
   projectChkBox();
-
+  fileUpload();
   /* Modify data before form submit */
   $(".btn-send").click(function (e) {
     e.preventDefault();
