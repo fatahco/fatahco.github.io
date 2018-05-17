7d42f721f4d2686217515d8a8af8bf35fe9567d4
Amjad Rathod <amjad@fatah.co>
2018-05-17 11:16:40 +0530

    add carousel in ablout page

--- assets/js/main.js
+++ assets/js/main.js
@@ -748,6 +748,72 @@ function ChangeNavColor() {
     });
   });
 }
+function Carousel() {
+  var speed = 5000;
+
+    var run = setInterval(rotate, speed);
+    var slides = $('.slide');
+    var container = $('#slides ul');
+    var elm = container.find(':first-child').prop("tagName");
+    var item_width = container.width();
+    var previous = 'prev'; //id of previous button
+    var next = 'next'; //id of next button
+    slides.width(item_width); //set the slides to the correct pixel width
+    container.parent().width(item_width);
+    container.width(slides.length * item_width); //set the slides container to the correct total width
+    container.find(elm + ':first').before(container.find(elm + ':last'));
+    resetSlides();
+
+
+    //if user clicked on prev button
+
+    $('#buttons a').click(function (e) {
+        //slide the item
+
+        if (container.is(':animated')) {
+            return false;
+        }
+        if (e.target.id == previous) {
+            container.stop().animate({
+                'left': 0
+            }, 1500, function () {
+                container.find(elm + ':first').before(container.find(elm + ':last'));
+                resetSlides();
+            });
+        }
+
+        if (e.target.id == next) {
+            container.stop().animate({
+                'left': item_width * -2
+            }, 1500, function () {
+                container.find(elm + ':last').after(container.find(elm + ':first'));
+                resetSlides();
+            });
+        }
+
+        //cancel the link behavior
+        return false;
+
+    });
+
+    //if mouse hover, pause the auto rotation, otherwise rotate it
+    container.parent().mouseenter(function () {
+        clearInterval(run);
+    }).mouseleave(function () {
+        run = setInterval(rotate, speed);
+    });
+
+
+    function resetSlides() {
+        //and adjust the container so current is in the frame
+        container.css({
+            'left': -1 * item_width
+        });
+    }
+    function rotate() {
+      $('#next').click();
+    }
+}
 function GenralPage() {
   menuOpenClose();
   menuLinkClick();
@@ -798,6 +864,10 @@ function CareerPage() {
   //   $(".copy_right").css('position','unset');
   // }
 }
+function AboutPage() {
+  Carousel();
+
+}
 function mainJS() {
   if (location.pathname == '/porbandar/'){
     porbandarPage();
@@ -816,6 +886,9 @@ function mainJS() {
   }
   else if (location.pathname == '/skills/'){
     SkillPage();
+  }
+  else if (location.pathname == '/about/'){
+    AboutPage();
   }
 }
 function currentUrl() {
