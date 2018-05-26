c6902b541dc5f69437d711f346058ac71d0a03d6
Amjad Rathod <amjad@fatah.co>
2018-05-26 12:39:19 +0530

    Remove old carousel code from Js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -134,7 +134,7 @@ function menuAnimation() {
       TweenLite.to($(menuLink), 0, {opacity:0, visibility: 'hidden', right:0});
       TweenLite.to($(logo), .5, {top:0, width:200, left:45});
       if (window.matchMedia('(max-width: 767px)').matches) {
-        TweenLite.to($(logo), .5, {top:0, width:200, left:20});
+        TweenLite.to($(logo), .5, {top:0, width:200, left:20
       }
     }
   });
@@ -570,72 +570,7 @@ function ChangeNavColor() {
     });
   });
 }
-function Carousel() {
-  var speed = 5000;
 
-    var run = setInterval(rotate, speed);
-    var slides = $('.slide');
-    var container = $('#slides ul');
-    var elm = container.find(':first-child').prop("tagName");
-    var item_width = container.width();
-    var previous = 'prev'; //id of previous button
-    var next = 'next'; //id of next button
-    slides.width(item_width); //set the slides to the correct pixel width
-    container.parent().width(item_width);
-    container.width(slides.length * item_width); //set the slides container to the correct total width
-    container.find(elm + ':first').before(container.find(elm + ':last'));
-    resetSlides();
-
-
-    //if user clicked on prev button
-
-    $('#buttons a').click(function (e) {
-        //slide the item
-
-        if (container.is(':animated')) {
-            return false;
-        }
-        if (e.target.id == previous) {
-            container.stop().animate({
-                'left': 0
-            }, 1500, function () {
-                container.find(elm + ':first').before(container.find(elm + ':last'));
-                resetSlides();
-            });
-        }
-
-        if (e.target.id == next) {
-            container.stop().animate({
-                'left': item_width * -2
-            }, 1500, function () {
-                container.find(elm + ':last').after(container.find(elm + ':first'));
-                resetSlides();
-            });
-        }
-
-        //cancel the link behavior
-        return false;
-
-    });
-
-    //if mouse hover, pause the auto rotation, otherwise rotate it
-    container.parent().mouseenter(function () {
-        clearInterval(run);
-    }).mouseleave(function () {
-        run = setInterval(rotate, speed);
-    });
-
-
-    function resetSlides() {
-        //and adjust the container so current is in the frame
-        container.css({
-            'left': -1 * item_width
-        });
-    }
-    function rotate() {
-      $('#next').click();
-    }
-}
 function HomePageAnimation() {
   $(".tag-line__hover.About").css({height: "25px", color: "#fff"});
   $(".tag-line__hover.About span").css({opacity: "1"});
