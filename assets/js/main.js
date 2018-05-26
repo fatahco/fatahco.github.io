88a24b1ec519315ebb6302517a96a00cc9d7ebb0
Amjad Rathod <amjad@fatah.co>
2018-05-26 12:30:05 +0530

    Remove old menu all code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,137 +1,4 @@
-// function menuOpenClose() {
-//   if ($ ('.toggle-menu').hasClass('menu-close')) {
-//     $('.toggle-menu').addClass('menu-open');
-//     $('.menu-item').addClass('menu-item-saw');
-//     $('.menu-item').removeClass('menu-item-hide');
-//     $('.menu-trangition').addClass('menu-trangition-open');
-//     $('.menu-trangition').removeClass('menu-trangition-close');
-//     $('.toggle-menu').removeClass('menu-close');
-//     $('.bar-middle').css({ 'opacity' : '0'});
-//     $('.bar-top').addClass('top-x');
-//     $('.bar-bottom').addClass('bottom-x');
-//     $('.bar-top').removeClass('top-hover');
-//     $('.bar-bottom').removeClass('bottom-hover');
-//     $('.box-line-left').removeClass('left-hover');
-//     $('.box-line-left').addClass('left');
-//     $('.box-line-right').addClass('right');
-//     $('.box-line-right').removeClass('right-hover');
-//     $('.brand-icon').addClass('open-menu-brand-icon');
-//     $('.brand-icon').removeClass('close-menu-brand-icon');
-//     setTimeout(function () {
-//       $('page-loader').addClass('open-loader');
-//       setTimeout(function () {
-//         $('page-loader').addClass('close-loader');
-//         $('page-loader').removeClass('open-loader');
-//       }, 500);
-//       setTimeout(function () {
-//         $('page-loader').removeClass('close-loader');
-//       }, 500);
-//     }, 500);
-//   }
-//   else if ($ ('.toggle-menu').hasClass('menu-open')) {
-//     $('.bar-top').removeClass('top-x');
-//     $('.bar-bottom').removeClass('bottom-x');
-//     $('.toggle-menu').removeClass('menu-open');
-//     $('.toggle-menu').addClass('menu-close');
-//     setTimeout(function () {
-//       $('.bar-top').addClass('top-hover');
-//       $('.bar-bottom').addClass('bottom-hover');
-//     }, 500);
-//       $('.box-line-right').removeClass('right');
-//       $('.box-line-left').removeClass('left');
-//       $('.box-line-left').addClass('left-hover');
-//       $('.box-line-right').addClass('right-hover');
-//     $('.bar-middle').css({ 'opacity' : '1'});
-//     $('.menu-trangition').addClass('menu-trangition-close');
-//     $('.menu-trangition').removeClass('menu-trangition-open');
-//     $('.menu-item').addClass('menu-item-hide');
-//     $('.menu-item').removeClass('menu-item-saw');
-//     $('.brand-icon').addClass('close-menu-brand-icon');
-//     $('.brand-icon').removeClass('open-menu-brand-icon');
-//   }
-// }
-// function menuLinkClick() {
-//
-//   $('.brand-icon a').click(function (e) {
-//     e.preventDefault();
-//     $('.header a').removeClass('active');
-//     $(this).addClass('active');
-//     if ($ ('.toggle-menu').hasClass('menu-open')) {
-//       $('.bar-top').removeClass('top-x');
-//       $('.bar-bottom').removeClass('bottom-x');
-//       $('.toggle-menu').removeClass('menu-open');
-//       $('.toggle-menu').addClass('menu-close');
-//       setTimeout(function () {
-//         $('.bar-top').addClass('top-hover');
-//         $('.bar-bottom').addClass('bottom-hover');
-//       }, 500);
-//         $('.box-line-right').removeClass('right');
-//         $('.box-line-left').removeClass('left');
-//         $('.box-line-left').addClass('left-hover');
-//         $('.box-line-right').addClass('right-hover');
-//       $('.bar-middle').css({ 'opacity' : '1'});
-//       $('.menu-trangition').addClass('menu-trangition-close');
-//       $('.menu-trangition').removeClass('menu-trangition-open');
-//       $('.menu-item').addClass('menu-item-hide');
-//       $('.menu-item').removeClass('menu-item-saw');
-//       $('.brand-icon').addClass('close-menu-brand-icon');
-//       $('.brand-icon').removeClass('open-menu-brand-icon');
-//     }
-//
-//   });
-//   $('.menu-item a').click(function (e) {
-//     e.preventDefault();
-//     $('.header a').removeClass('active');
-//     $(this).addClass('active');
-//     menuOpenClose();
-//   });
-// }
-// function menuBarClick() {
-//   if ( $('.toggle-menu').hasClass('menu-open') ) {
-//     $('.bar-top').removeClass('top-x');
-//     $('.bar-bottom').removeClass('bottom-x');
-//     $('.toggle-menu').removeClass('menu-open');
-//     $('.toggle-menu').addClass('menu-close');
-//     setTimeout(function () {
-//       $('.bar-top').addClass('top-hover');
-//       $('.bar-bottom').addClass('bottom-hover');
-//     }, 500);
-//     $(".menu-bar").one("mouseleave", function() {
-//       if ( !$('.toggle-menu').hasClass('menu-open') ) {
-//         $('.box-line-right').removeClass('right');
-//         $('.box-line-left').removeClass('left');
-//         $('.box-line-left').addClass('left-hover');
-//         $('.box-line-right').addClass('right-hover');
-//       }
-//     });
-//     $('.bar-middle').css({ 'opacity' : '1'});
-//     $('.menu-trangition').addClass('menu-trangition-close');
-//     $('.menu-trangition').removeClass('menu-trangition-open');
-//     $('.menu-item').addClass('menu-item-hide');
-//     $('.menu-item').removeClass('menu-item-saw');
-//     $('.brand-icon').addClass('close-menu-brand-icon');
-//     $('.brand-icon').removeClass('open-menu-brand-icon');
-//   }
-//   else {
-//     $('.toggle-menu').addClass('menu-open');
-//     $('.menu-item').addClass('menu-item-saw');
-//     $('.menu-item').removeClass('menu-item-hide');
-//     $('.menu-trangition').addClass('menu-trangition-open');
-//     $('.menu-trangition').removeClass('menu-trangition-close');
-//     $('.toggle-menu').removeClass('menu-close');
-//     $('.bar-middle').css({ 'opacity' : '0'});
-//     $('.bar-top').addClass('top-x');
-//     $('.bar-bottom').addClass('bottom-x');
-//     $('.bar-top').removeClass('top-hover');
-//     $('.bar-bottom').removeClass('bottom-hover');
-//     $('.box-line-left').removeClass('left-hover');
-//     $('.box-line-left').addClass('left');
-//     $('.box-line-right').addClass('right');
-//     $('.box-line-right').removeClass('right-hover');
-//     $('.brand-icon').addClass('open-menu-brand-icon');
-//     $('.brand-icon').removeClass('close-menu-brand-icon');
-//   }
-// }
+
 function menuAnimation() {
   var menuIcon = $('.menu-bar'),
   middleBar = $('.bar-middle'),
@@ -969,34 +836,6 @@ function HomePageAnimation() {
     });
   }
 }
-// function BrowserButton() {
-//   $(window).on('popstate', function(event) {
-//     if ($ ('.toggle-menu').hasClass('menu-open')) {
-//       $('.bar-top').removeClass('top-x');
-//       $('.bar-bottom').removeClass('bottom-x');
-//       $('.toggle-menu').removeClass('menu-open');
-//       // $('.toggle-menu').removeClass('menu-open');
-//       $('.toggle-menu').addClass('menu-close');
-//       setTimeout(function () {
-//         // $('.box-line-right').removeClass('right');
-//         // $('.box-line-left').removeClass('left');
-//         $('.bar-top').addClass('top-hover');
-//         $('.bar-bottom').addClass('bottom-hover');
-//       }, 500);
-//         $('.box-line-right').removeClass('right');
-//         $('.box-line-left').removeClass('left');
-//         $('.box-line-left').addClass('left-hover');
-//         $('.box-line-right').addClass('right-hover');
-//       $('.bar-middle').css({ 'opacity' : '1'});
-//       $('.menu-trangition').addClass('menu-trangition-close');
-//       $('.menu-trangition').removeClass('menu-trangition-open');
-//       $('.menu-item').addClass('menu-item-hide');
-//       $('.menu-item').removeClass('menu-item-saw');
-//       $('.brand-icon').addClass('close-menu-brand-icon');
-//       $('.brand-icon').removeClass('open-menu-brand-icon');
-//     }
-//   });
-// }
 
 function MyCaousel() {
   $(".client_comments").slick({
@@ -1025,18 +864,10 @@ function MyCaousel() {
   });
 }
 function GenralPage() {
-  // menuOpenClose();
-  // menuLinkClick();
   menuAnimation();
   HomePageAnimation();
   ChangeNavColor();
-  // BrowserButton();
-  // menuBarClick();
-
   removeHref();
-  // if ($('.menu-bar').click()) {
-  //   $('.menu-bar').bind( "click", menuBarClick );
-  // }
 }
 function HomePage() {
   $(".copy_right").css({'position':'absolute', 'background':'#fff', 'color':'#000'});
